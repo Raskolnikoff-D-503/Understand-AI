4e69ea6e5066421ebdf6d06beace060b45c0b21a
Raskolnikoff-D-503 <gaagevgenii@gmail.com>
2023-03-23 23:28:37 +0300

    Added drop hook

--- src/components/DraggableElement/DraggableElement.tsx
+++ src/components/DraggableElement/DraggableElement.tsx
@@ -1,21 +1,44 @@
 import React, {ReactNode, useEffect} from 'react';
-import {DragSourceMonitor, useDrag} from 'react-dnd';
+import {useDrag, useDrop} from 'react-dnd';
 import {getEmptyImage} from 'react-dnd-html5-backend';
 
 type Props = {
+  id: number;
+  moveCard: (id: number, to: number) => void;
+  findCard: (id: number) => {index: number};
   children: ReactNode;
 };
 
-export const DraggableElement = ({children}: Props) => {
+export const DraggableElement = ({id, moveCard, findCard, children}: Props) => {
+  const originalIndex = findCard(id).index;
   const [{opacity}, dragRef, preview] = useDrag(
     () => ({
       type: 'card',
       item: children,
-      collect: (monitor: DragSourceMonitor) => ({
+      collect: (monitor) => ({
         opacity: monitor.isDragging() ? 0 : 1,
       }),
+      end: (_, monitor) => {
+        const didDrop = monitor.didDrop();
+        if (!didDrop) {
+          moveCard(id, originalIndex);
+        }
+      },
     }),
-    [],
+    [id, originalIndex, moveCard],
+  );
+
+  const [, dropRef] = useDrop(
+    () => ({
+      accept: 'card',
+      hover() {
+        if (id !== id) {
+          const {index: overIndex} = findCard(id);
+          moveCard(id, overIndex);
+        }
+      },
+    }),
+    [findCard, moveCard],
   );
 
   useEffect(() => {
@@ -23,7 +46,11 @@ export const DraggableElement = ({children}: Props) => {
   }, []);
 
   return (
-    <div ref={dragRef} className="draggable-element" style={{opacity, cursor: 'grab'}}>
+    <div
+      ref={(node) => dragRef(dropRef(node))}
+      className="draggable-element"
+      style={{opacity, cursor: 'grab'}}
+    >
       {children}
     </div>
   );
