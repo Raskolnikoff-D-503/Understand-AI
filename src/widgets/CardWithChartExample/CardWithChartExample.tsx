b154212e782aa16c7923fe99fab874b5bd657e54
Evgenii Gaag <gaagevgenii@gmail.com>
2023-04-25 20:22:26 +0300

    configurated responsive design, added fonts

--- src/widgets/CardWithChartExample/CardWithChartExample.tsx
+++ src/widgets/CardWithChartExample/CardWithChartExample.tsx
@@ -1,8 +1,7 @@
 import React from 'react';
 import {Chart as ChartJS, ArcElement, Tooltip, Legend, ChartOptions, ChartData} from 'chart.js';
 import {Doughnut} from 'react-chartjs-2';
-import {Card, Title} from '@/shared/UI';
-import {SIZE} from '@/shared/constants';
+import {Card} from '@/shared/UI';
 
 import './CardWithChartExample.scss';
 
@@ -28,13 +27,17 @@ const options: ChartOptions = {
   responsive: true,
   maintainAspectRatio: true,
   plugins: {legend: {position: 'right'}},
-  aspectRatio: 2,
+  aspectRatio: 1,
 };
 
 export const CardWithChartExample = ({id, className}: Props) => {
   return (
-    <Card id={id} className={`card-with-chart ${className}`}>
-      <Title size={SIZE.SMALL}>Some Chart With Important Data</Title>
+    <Card
+      id={id}
+      className={`card-with-chart ${className}`}
+      title="Some Chart With Important Data"
+      isDraggable
+    >
       <div className="card-with-chart__container">
         <Doughnut data={data} options={options} />
       </div>
