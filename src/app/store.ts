41a06e69f394bd6cf3f181a0f81698bd9682bea5
Evgenii Gaag <gaagevgenii@gmail.com>
2023-04-27 20:43:56 +0300

    got rid of unused google api

--- src/app/store.ts
+++ src/app/store.ts
@@ -1,18 +1,13 @@
 import {configureStore} from '@reduxjs/toolkit';
 import {setupListeners} from '@reduxjs/toolkit/dist/query';
 import {rootReducer} from './rootReducer';
-import {googleNewsApi} from './services/googleNews/api';
 import {yahooFinanceApi} from './services/yahooFinance/api';
 import {AINewsApi} from './services/AINews/api';
 
 export const store = configureStore({
   reducer: rootReducer,
   middleware: (getDefaultMiddleware) =>
-    getDefaultMiddleware().concat([
-      googleNewsApi.middleware,
-      yahooFinanceApi.middleware,
-      AINewsApi.middleware,
-    ]),
+    getDefaultMiddleware().concat([yahooFinanceApi.middleware, AINewsApi.middleware]),
 });
 
 setupListeners(store.dispatch);
