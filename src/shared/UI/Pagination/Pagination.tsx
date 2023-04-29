4a13aff8c69684070db41124e526a98c312dc4a2
Evgenii Gaag <gaagevgenii@gmail.com>
2023-04-29 16:17:26 +0300

    worked on Pagination component, changed font sizes for paragraphs and titles

--- src/shared/UI/Pagination/Pagination.tsx
+++ src/shared/UI/Pagination/Pagination.tsx
@@ -10,8 +10,6 @@ type Props = {
 };
 
 export const Pagination = ({currentPage, nextPage = 0, onPageChange, className = ''}: Props) => {
-  console.log('pagination', currentPage, nextPage, onPageChange);
-
   if (currentPage === 0) {
     return null;
   }
@@ -26,14 +24,10 @@ export const Pagination = ({currentPage, nextPage = 0, onPageChange, className =
 
   return (
     <ul className={`pagination-container ${className}`}>
-      {/* Left navigation arrow */}
       <li className={`pagination-item ${currentPage === 1 ? 'disabled' : ''}`} onClick={onPrevious}>
         <div className="arrow left" />
       </li>
-      <li className="pagination-item" onClick={() => onPageChange(currentPage)}>
-        {currentPage}
-      </li>
-      {/*  Right Navigation arrow */}
+      <li className="pagination-item">{currentPage}</li>
       <li className={`pagination-item ${!nextPage ? 'disabled' : ''}`} onClick={onNext}>
         <div className="arrow right" />
       </li>
