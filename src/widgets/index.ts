fcd3c55bd6e47a4fdfc4655b6d43f1b5ccf5f9e2
Evgenii Gaag <gaagevgenii@gmail.com>
2023-04-30 20:25:08 +0300

    added accordion component, icon for accordion, applied styles, created widget for saved chat gpt replies

--- src/widgets/index.ts
+++ src/widgets/index.ts
@@ -2,3 +2,4 @@ export {Header} from './Header/Header';
 export {LearningResourcesWidget} from './LearningResourcesWidget/LearningResourcesWidget';
 export {ChatGPTWidget} from './ChatGPTWidget/ChatGPTWidget';
 export {CardWithChartExample} from './CardWithChartExample/CardWithChartExample';
+export {SavedChatGPTRepliesWidget} from './SavedChatGPTRepliesWidget/SavedChatGPTRepliesWidget';
