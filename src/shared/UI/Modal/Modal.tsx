a478f26c1cdb4539f13ce486ef3ec7307e2ecabc
Evgenii Gaag <gaagevgenii@gmail.com>
2023-05-19 12:32:38 +0300

    added functionality to edit response, created entity folder, ResponseForm entity and features to create and edit response, improved Modal component, added new shared utils

--- src/shared/UI/Modal/Modal.tsx
+++ src/shared/UI/Modal/Modal.tsx
@@ -5,18 +5,26 @@ import {createPortal} from 'react-dom';
 
 type Props = {
   id: string;
+  isOpen?: boolean;
   children: ReactNode;
   handleModalClose: () => void;
 };
 
-export const Modal = ({id, children, handleModalClose}: Props) => {
+export const Modal = ({id, isOpen = false, children, handleModalClose}: Props) => {
   useEffect(() => {
-    document.body.classList.add('overflow-hidden');
+    if (isOpen) {
+      document.body.classList.add('overflow-hidden');
+    }
 
     return () => {
       document.body.classList.remove('overflow-hidden');
     };
-  }, []);
+  }, [isOpen]);
+
+  if (!isOpen) {
+    return null;
+  }
+
   return createPortal(
     <div id={id} className="modal__backdrop" onClick={handleModalClose}>
       <div className="modal__content" onClick={(e) => e.stopPropagation()}>
