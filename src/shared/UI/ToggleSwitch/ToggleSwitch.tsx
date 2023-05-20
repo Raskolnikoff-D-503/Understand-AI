f9ae96da2c0e6608df81c393d33bfcc558c9c74c
Evgenii Gaag <gaagevgenii@gmail.com>
2023-05-20 23:18:12 +0300

    addded functionality to disabled toggle switch in widgets when widgets are draggable

--- src/shared/UI/ToggleSwitch/ToggleSwitch.tsx
+++ src/shared/UI/ToggleSwitch/ToggleSwitch.tsx
@@ -5,11 +5,12 @@ import './ToggleSwitch.scss';
 type Props = {
   isToggled: boolean;
   onToggle: () => void;
+  disabled?: boolean;
 };
 
-export const ToggleSwitch = ({isToggled = false, onToggle}: Props) => {
+export const ToggleSwitch = ({isToggled = false, onToggle, disabled = false}: Props) => {
   return (
-    <label className="toggle-switch">
+    <label className={`toggle-switch ${disabled ? 'toggle-switch--disabled' : ''}`}>
       <input type="checkbox" checked={isToggled} onChange={onToggle} />
       <span className="slider" />
     </label>
