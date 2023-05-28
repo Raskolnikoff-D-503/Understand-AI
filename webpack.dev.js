dbf92912e231bd6830740488f8d17622617cd66e
Evgenii Gaag <gaagevgenii@gmail.com>
2023-05-28 20:14:55 +0300

    webpack configuration

---
+++ webpack.dev.js
@@ -0,0 +1,17 @@
+const {merge} = require('webpack-merge');
+const common = require('./webpack.common.js');
+const path = require('path');
+
+module.exports = merge(common, {
+  mode: 'development',
+  devtool: 'inline-source-map',
+  devServer: {
+    static: {
+      directory: path.join(__dirname, 'dist'),
+    },
+    compress: true,
+    open: false,
+    port: 3000,
+    historyApiFallback: true,
+  },
+});
