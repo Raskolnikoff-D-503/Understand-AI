b2a19bf4f24f3c25b32afed45c68c487e65b5e38
Evgenii Gaag <gaagevgenii@gmail.com>
2023-05-29 13:25:21 +0300

    removed unused library

--- src/widgets/ChatGPTWidget/api/api.ts
+++ src/widgets/ChatGPTWidget/api/api.ts
@@ -1,7 +1,5 @@
 import {createApi, fetchBaseQuery} from '@reduxjs/toolkit/query/react';
 
-// const {REACT_APP_RAPID_API_KEY} = process.env;
-
 type MessageChatGPT = {
   role: string;
   content: string;
